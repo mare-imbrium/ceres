41507665b2c35a025b49d8cbc3a4919ae3d44a55
kepler <githubkepler.50s@gishpuppy.com>
2019-05-18 11:12:22 +0530

    move colorparser to another file

--- src/cet.cr
+++ src/cet.cr
@@ -6,12 +6,13 @@
 #       Author: jkepler http://github.com/jkepler/
 #         Date: 2019-05-01
 #      License: MIT
-#  Last update: 2019-05-18 09:23
+#  Last update: 2019-05-18 11:07
 # --------------------------------------------------------------------------- #
 # == CHANGELOG
+# renamed grows to max_items, gcols and glines to columns and lines
 
 #  == TODO
-# renamed grows to max_items, gcols and glines to columns and lines
+# 2019-05-18 - move selection, menu, colorparser out
 
 require "readline"
 require "file_utils"
@@ -21,8 +22,15 @@ require "logger"
 require "./keyhandler"
 require "./screen"
 require "./directory"
+require "./colorparser"
 
 SEPARATOR = "-------"
+RED       = "\e[31m"
+MAGENTA   = "\e[35m"
+CYAN      = "\e[36m"
+GREEN     = "\e[32m"
+BLUE      = "\e[1;34m"
+
 module Cet
   class Cetus
     # # GLOBALS
@@ -54,15 +62,10 @@ module Cet
     CLEAR     = "\e[0m"
     BOLD      = "\e[1m"
     BOLD_OFF  = "\e[22m"
-    RED       = "\e[31m"
     ON_RED    = "\e[41m"
     ON_GREEN  = "\e[42m"
     ON_YELLOW = "\e[43m"
-    GREEN     = "\e[32m"
     YELLOW    = "\e[33m"
-    BLUE      = "\e[1;34m"
-    MAGENTA   = "\e[35m"
-    CYAN      = "\e[36m"
 
     ON_BLUE      = "\e[44m"
     REVERSE      = "\e[7m"
@@ -70,38 +73,6 @@ module Cet
     UNDERLINE    = "\e[4m"
     CURSOR_COLOR = REVERSE
 
-    # NOTE: that osx uses LSCOLORS which only has colors for filetypes not
-    #  extensions and patterns which LS_COLORS has.
-    # LS_COLORS contains 2 character filetype colors. ex executable mi broken link
-    #   extension based colros starting with '*.'
-    #   file pattern starting with '*' and a character that is not .
-    #   File.ftype(path) returns
-    #   file, directory di, characterSpecial cd, blockSpecial bd, fifo pi, link ln, socket so, or unknown
-
-    # This hash contains color codes for extensions. It is updated from
-    # LS_COLORS.
-    @ls_color = {
-      ".rb"      => RED,
-      ".tgz"     => MAGENTA,
-      ".zip"     => MAGENTA,
-      ".torrent" => GREEN,
-      ".srt"     => GREEN,
-      ".part"    => "\e[40;31;01m",
-      ".sh"      => CYAN,
-    }
-    # This hash contains colors for file patterns, updated from LS_COLORS
-    @ls_pattern = {} of String => String
-    # This hash contains colors for file types, updated from LS_COLORS
-    # Default values in absence of LS_COLORS
-    # crystal sends Directory, with initcaps, Symlink, CharacterDevice, BlockDevice
-    # Pipe, Socket and Unknown https://crystal-lang.org/api/0.28.0/File/Type.html
-    @ls_ftype = {
-      "Directory" => BLUE,
-      "Symlink"   => "\e[01;36m",
-      "mi"        => "\e[01;31;7m",
-      "or"        => "\e[40;31;01m",
-      "ex"        => "\e[01;32m",
-    }
     # @log = Logger.new(File.expand_path("~/tmp/log.txt"))
     @@log = Logger.new(io: File.new(File.expand_path("log.txt"), "w"))
     @@log.level = Logger::DEBUG
@@ -193,6 +164,7 @@ module Cet
       @columns       = 0
       @columns       = @screen.columns
       @directory     = Directory.new
+      @colorparser   = Colorparser.new
       @selected_files = [] of String
       @files = [] of String
       @view = [] of String
@@ -963,7 +935,7 @@ module Cet
       prefix = "#{ind}#{mark}#{cur}"
 
       # fullname = f[0] == '~' ? File.expand_path(f) : f
-      color = color_for(f)
+      color = @colorparser.color_for(f)
       # @@log.debug "Color is #{color[1..-2]} for #{f}" if color
       color = "#{bcolor}#{color}"
 
@@ -1055,110 +1027,6 @@ module Cet
       return f
     end
 
-    # determine color for a filename based on extension, then pattern, then filetype
-    def color_for(f)
-      return nil if f == SEPARATOR
-
-      fname = f.starts_with?("~/") ? File.expand_path(f) : f
-
-      extension = File.extname(fname)
-      color = @ls_color[extension]?
-      return color if color
-
-      # check file against patterns
-      if File.file?(fname)
-         @ls_pattern.each do |k, v|
-           # if fname.match(/k/)
-           if /#{k}/.match(fname)
-              @@log.debug "#{fname} matched #{k}. color is #{v[1..-2]}"
-             return v
-              # else
-              # @@log.debug "#{fname} di not match #{k}. color is #{v[1..-2]}"
-           end
-         end
-      end
-
-      # check filetypes
-      if File.exists? fname
-
-        ftype = File.info(fname).type.to_s # it was File::Type thus not matching
-        # @@log.debug "Filetype:#{ftype}, #{ftype.class}."
-        # CRYSTAL ftype
-        return @ls_ftype[ftype]? if @ls_ftype.has_key?(ftype)
-        # @@log.debug "went past ftype for #{fname}"
-        return @ls_ftype["ex"]? if File.executable?(fname)
-      else
-        # orphan file, but fff uses mi
-        return @ls_ftype["mi"]? if File.symlink?(fname)
-
-        @@log.warn "FILE WRONG: #{fname}"
-        return @ls_ftype["or"]?
-      end
-
-      nil
-    end
-
-    def parse_ls_colors
-      colorvar = ENV["LS_COLORS"]?
-      if colorvar.nil?
-        @ls_colors_found = nil
-        return
-      end
-      @ls_colors_found = true
-      ls = colorvar.split(":")
-      ls.each do |e|
-        next if e == ""
-        patt, colr = e.split "=" # IOOB CRYSTAL, split throws error if blank
-        colr = "\e[" + colr + "m"
-        if e.starts_with? "*."
-          # extension, avoid '*' and use the rest as key
-          @ls_color[patt[1..-1]] = colr
-          # @@log.debug "COLOR: Writing extension (#{patt})."
-        elsif e[0] == '*'
-          # file pattern, this would be a glob pattern not regex
-          # only for files not directories
-          patt = patt.gsub(".", "\.")
-          patt = patt.sub("+", "\\\+") # if i put a plus it does not go at all
-          patt = patt.gsub("-", "\-")
-          patt = patt.tr("?", ".")
-          patt = patt.gsub("*", ".*")
-          patt = "^#{patt}" if patt[0] != "."
-          patt = "#{patt}$" if patt[-1] != "*"
-          @ls_pattern[patt] = colr
-          # @@log.debug "COLOR: Writing file (#{patt})."
-        elsif patt.size == 2
-          # file type, needs to be mapped to what ruby will return
-          # file, directory di, characterSpecial cd, blockSpecial bd, fifo pi, link ln, socket so, or unknown
-          # di = directory
-          # fi = file
-          # ln = symbolic link
-          # pi = fifo file
-          # so = socket file
-          # bd = block (buffered) special file
-          # cd = character (unbuffered) special file
-          # or = symbolic link pointing to a non-existent file (orphan)
-          # mi = non-existent file pointed to by a symbolic link (visible when you type ls -l)
-          # ex = file which is executable (ie. has 'x' set in permissions).
-          case patt
-          when "di"
-            @ls_ftype["Directory"] = colr
-          when "cd"
-            @ls_ftype["CharacterDevice"] = colr
-          when "bd"
-            @ls_ftype["BlockDevice"] = colr
-          when "pi"
-            @ls_ftype["Pipe"] = colr
-          when "ln"
-            @ls_ftype["Symlink"] = colr
-          when "so"
-            @ls_ftype["Socket"] = colr
-          else
-            @ls_ftype[patt] = colr
-          end
-          # @@log.debug "COLOR: ftype #{patt}"
-        end
-      end
-    end
 
     # # select file based on key pressed
     def select_hint(view, key)
@@ -3616,7 +3484,7 @@ module Cet
 
       @screen.setup_terminal
       config_read
-      parse_ls_colors # FIXME TODO
+      @colorparser.parse_ls_colors # FIXME TODO
       set_bookmark "0"
 
       redraw true
