bca8169c2ebf8c8827c965b0754396f7f4608a15
kepler <githubkepler.50s@gishpuppy.com>
2019-05-19 19:07:32 +0530

    initial commit

---
+++ src/colorconstants.cr
@@ -0,0 +1,22 @@
+module ColorConstants
+
+  RED       = "\e[31m"
+  MAGENTA   = "\e[35m"
+  CYAN      = "\e[36m"
+  GREEN     = "\e[32m"
+  BLUE      = "\e[1;34m"
+
+  CLEAR     = "\e[0m"
+  BOLD      = "\e[1m"
+  BOLD_OFF  = "\e[22m"
+  ON_RED    = "\e[41m"
+  ON_GREEN  = "\e[42m"
+  ON_YELLOW = "\e[43m"
+  YELLOW    = "\e[33m"
+
+  ON_BLUE      = "\e[44m"
+  REVERSE      = "\e[7m"
+  REVERSE_OFF  = "\e[27m"
+  UNDERLINE    = "\e[4m"
+  CURSOR_COLOR = REVERSE
+end
